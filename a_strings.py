cc0b4758a7807620146fc0ee9a04b32b42d550bc
Nikita Volokitin <142596413+vvvolokitin@users.noreply.github.com>
2024-11-14 22:05:08 +0500

    Add files via upload

---
+++ a_strings.py
@@ -0,0 +1,33 @@
+name: str = 'Иван'
+
+# last_name: str = input('Введите фамилию: ')
+# print(name, last_name)
+
+last_name = 'Иванов'
+
+# # Конкатенация
+# full_name: str = name + ' ' + last_name
+# print(full_name)
+
+# Индексация
+# print(name)
+# print(name[0], name[-1])
+
+# #Срезы
+# print(f'Срез с 0 до 3 - {name[0:3]}')
+# print(f'Срез без указания начального индекса до 3 - {name[:3]}')
+# print(f'Срез cо 2 индекса и до конца - {name[2:]}')
+# print(f'Срез с шагом 2 от 0 до 3 - {name[0:3:2]}')
+# print(f'Срез с шагом -1 - {name[::-1]}')
+
+# # # методы
+word: str = 'Слово'
+
+print(word.upper())
+print(word.lower())
+print(word.count('о'))
+print(word[::-1].capitalize())
+print(word.replace('о', 'а'))
+
+
+
